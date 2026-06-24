bfe9faee092a8119e5b4ec49928ab850b33f1113
sm3031972-sys <sm3031972@gmail.com>
2026-06-24 18:37:25 +0530

    Time: 5 ms (36.87%), Space: 43.8 MB (13.47%) - LeetHub

---
+++ 1051-height-checker/1051-height-checker.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+class Solution {
+    public int heightChecker(int[] heights) {
+        int[] expected = heights.clone();
+        
+        
+        Arrays.sort(expected);
+        
+        int count = 0;
+        
+        
+        for (int i = 0; i < heights.length; i++) {
+            if (heights[i] != expected[i]) {
+                count++;
+            }
+        }
+        
+        return count;
+    }
+    
+}
+
+
