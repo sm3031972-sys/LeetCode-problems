97b23300cdd7fe1bf167339410edbde3a9c37f4f
sm3031972-sys <sm3031972@gmail.com>
2026-07-07 08:48:15 +0530

    Time: 1 ms (94.63%), Space: 43.2 MB (93.85%) - LeetHub

---
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String gcdOfStrings(String str1, String str2) {
+        int len1 = str1.length() , len2 = str2.length();
+        if(!(str1+str2).equals(str2 + str1)){
+            return "";
+        }
+        String result = str1.substring(0,gcd(len1,len2));
+        return result;
+    }
+    private int gcd(int num1,int num2){
+        if(num2 == 0){
+            return num1;
+        }
+        return gcd(num2 , num1 % num2);
+    }
+}
