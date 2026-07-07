62d52d88a3f5371dd061ce33a3175adc1d60f6da
sm3031972-sys <sm3031972@gmail.com>
2026-07-07 09:29:02 +0530

    Time: 3 ms (39.63%), Space: 44.1 MB (7.87%) - LeetHub

--- 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.java
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.java
@@ -4,12 +4,12 @@ class Solution {
             return false;
         }
         Map<Integer,Integer> map = new HashMap<>();
-        for(int num : arr){
-            if(map.containsKey(num)){
-                map.put(num , map.get(num) + 1);
+        for(int i=0;i<arr.length;i++){
+            if(map.containsKey(arr[i])){
+                map.put(arr[i] , map.get(arr[i]) + 1);
             }
             else{
-                map.put(num , 1);
+                map.put(arr[i] , 1);
             }
         }
         Set<Integer> set = new HashSet<>(map.values());
