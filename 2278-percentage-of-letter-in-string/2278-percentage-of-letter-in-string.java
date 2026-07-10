70a9791862199a35a63e8f09f01597ee5f931a3d
sm3031972-sys <sm3031972@gmail.com>
2026-07-10 16:56:52 +0530

    Time: 0 ms (100%), Space: 42.8 MB (40.73%) - LeetHub

---
+++ 2278-percentage-of-letter-in-string/2278-percentage-of-letter-in-string.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int percentageLetter(String s, char letter) {
+        char chars[] = s.toCharArray();
+        int count = 0;
+        for(int i=0;i<chars.length;i++){
+            if(chars[i] == letter){
+                count++;
+            }
+        }
+        int len = s.length();
+        // int percentage = (int)(count/len)*100;
+        return (int) ((count * 100.0) / len);
+    }
+}
