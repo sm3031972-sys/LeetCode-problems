e00e4696241da0448187df0c6223044f81ed3011
sm3031972-sys <sm3031972@gmail.com>
2026-07-16 10:43:45 +0530

    Time: 1 ms (21.46%), Space: 43.3 MB (99.61%) - LeetHub

---
+++ 3028-ant-on-the-boundary/3028-ant-on-the-boundary.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int returnToBoundaryCount(int[] nums) {
+        int count = 0;
+        int sum = 0;
+        for(int i=0;i<nums.length;i++){
+            sum += nums[i];
+            if(sum == 0){
+                count ++;
+            }
+        }    
+        return count;
+    }
+}
